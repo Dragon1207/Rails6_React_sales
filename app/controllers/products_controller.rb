61dca15f20bb12ce734abb521e3389894bd93797
Dragon1207 <smileboth0805@outlook.com>
2020-03-21 11:12:06 -0700

    Implemented new product functionality

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,11 +4,25 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
-  def show    
+  def show
     @product = Product.find(params[:id])
   end
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.save
+    redirect_to root_path
+  end
+
   private
+
+  def product_params
+    params.require(:product).permit([:name, :price, :description, :image_url])
+  end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
-  get '/products/:id', to: 'products#show', as: 'product'
-  get '/products', to: 'products#index'
 
+  get '/products',     to: 'products#index'
+  get '/products/new', to: 'products#new', as: 'new_product'
+  get '/products/:id', to: 'products#show', as: 'product'
+  post '/products',    to: 'products#create'
 
 end
