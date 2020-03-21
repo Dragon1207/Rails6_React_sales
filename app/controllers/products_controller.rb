77af5e15a668443ed20ccfe9dffc12d06845646b
Dragon1207 <smileboth0805@outlook.com>
2020-03-21 16:32:54 -0700

    Added flash messages and error handling

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,20 +14,31 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
-    @product.save
-    redirect_to root_path
+    if @product.save
+      flash[:notice] = 'Product has been created'
+      redirect_to root_path
+    else
+      flash.now[:alert] = 'There was an error, please try again'
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
-    @product.update(product_params)
-    redirect_to root_path
+    if @product.update(product_params)
+      flash[:notice] = 'Product has been updated'
+      redirect_to root_path
+    else
+      flash.now[:alert] = 'There was an error, please try again'
+      render :edit
+    end
   end
 
   def destroy
     @product.destroy
+    flash[:alert] = 'Product has been deleted'
     redirect_to root_path
   end
 
