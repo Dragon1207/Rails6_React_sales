d7ee7adcc6901934f92c3bc36241b5bb1a4d47d5
Dragon1207 <smileboth0805@outlook.com>
2020-03-20 17:16:17 -0700

    Implemented the products show action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,10 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
+  def show    
+    @product = Product.find(params[:id])
+  end
+
   private
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  # root to: 'products#index'
 
   root to: 'products#index'
+
+  get '/products/:id', to: 'products#show', as: 'product'
+  get '/products', to: 'products#index'
 
 
 end
