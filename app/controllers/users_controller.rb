5359301d6f0c62dc2acdf8ccaf5e5cac2dacf7fb
Dragon1207 <smileboth0805@outlook.com>
2020-03-22 16:18:32 -0700

    Completed user signup functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,20 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = 'Sign up successfully'
+      redirect_to root_path
+    else
+      flash.now[:alert] = 'There was an error'
+      render :new
+    end
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
+
+  has_secure_password
   validates :first_name, :last_name, :password, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false },
   format: {
       with: /\A[A-Z0-9#-_~!$&'()*+,;=:.]+@[A-Z0-9.-]+\.[A-Z]{2,4}\z/i }
 
-  has_secure_password
+
   before_save :downcase_email
 
   private
