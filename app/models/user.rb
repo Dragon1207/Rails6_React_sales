bc224dbdcc2a12ae7952d2647e2fe13474d1e021
Dragon1207 <smileboth0805@outlook.com>
2020-03-22 11:44:29 -0700

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ApplicationRecord
+  validates :first_name, :last_name, :password, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false },
+  format: {
+      with: /\A[A-Z0-9#-_~!$&'()*+,;=:.]+@[A-Z0-9.-]+\.[A-Z]{2,4}\z/i }
+
   has_secure_password
+  before_save :downcase_email
+
+  private
+
+  def downcase_email
+    self.email.downcase!
+  end
 end
