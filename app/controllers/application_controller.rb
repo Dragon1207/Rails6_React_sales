57674718ac154e61ff09e43e4ab6b2cbfe943f5e
Dragon1207 <smileboth0805@outlook.com>
2020-03-23 16:54:41 -0700

    Set user authorizations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,12 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find_by(id: cookies.signed[:user_id])
   end
 
+  def require_signin
+    unless current_user
+      flash[:alert] = 'Please sign in first'
+      redirect_to signin_path
+    end 
+  end
+
   helper_method :current_user
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  before_action :require_signin, except: [:index, :show]
   before_action :find_product, only: [:show, :edit, :update, :destroy]
 
   def index
