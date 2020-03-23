f5a677a5092789e17857f9524aac1c2a1251f7fe
Dragon1207 <smileboth0805@outlook.com>
2020-03-23 14:33:38 -0700

    Refactor error messages for products and users into partial

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 
   has_secure_password
-  validates :first_name, :last_name, :password, presence: true
+  validates :first_name, :last_name,  presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false },
   format: {
       with: /\A[A-Z0-9#-_~!$&'()*+,;=:.]+@[A-Z0-9.-]+\.[A-Z]{2,4}\z/i }
