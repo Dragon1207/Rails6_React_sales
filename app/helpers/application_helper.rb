e17096339a1b787b7443cd494fb754aa9d09a0b0
Dragon1207 <smileboth0805@outlook.com>
2020-03-24 15:08:36 -0700

    Refactor logic for rejecting comments into a helper method and add scope for comments order

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
   def commented_by(user)
     user.fullname
   end
+
+  def persisted_comments(comments)
+    comments.reject { |comment| comment.new_record? }
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,6 @@ class Comment < ApplicationRecord
   validates :body, presence: true
   belongs_to :product
   belongs_to :user
+
+  default_scope { order(created_at: :desc) }
 end
