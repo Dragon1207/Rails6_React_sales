97ebb76e7f995edeaf155805ccacf7d2a5c30e45
Dragon1207 <smileboth0805@outlook.com>
2020-03-25 13:41:45 -0700

    Complete Jumbotron and ProductList

--- app/javascript/components/products/Jumbotron.jsx
+++ app/javascript/components/products/Jumbotron.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
 const Jumbotron = () => (
-  <div>Jumbotron Component</div>
+  <div className="container">
+    <div className="row">
+      <div className="jumbotron">
+        <h1 className="display-4">Welcome to <span className="goog">O-Sale</span> online market place!</h1>
+        <p className="lead">Lorem ipsum dolor sit amet,consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+    </div>
+  </div>
 )
 
 
