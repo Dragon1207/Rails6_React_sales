066c0139555a05bfc63ab819a0d11121b7417134
Dragon1207 <smileboth0805@outlook.com>
2020-03-26 16:28:36 -0700

    Add the link component to products and header

--- app/javascript/components/products/Product.jsx
+++ app/javascript/components/products/Product.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 const Product = ({ product }) => (
   <div className="col-md-4 col-sm-6 col-xs-12 mb-4">
@@ -8,7 +9,7 @@ const Product = ({ product }) => (
       <div className="card-body">
         <h5 className="card-title">
           <span className="badge badge-pill badge-purple float-right">{product.price}</span>
-          <a href="detail-page.html">{product.name}</a>
+          <Link to={'/detail'}>{ product.name }</Link>          
         </h5>
         <p className="card-text">
             {product.description}
--- app/javascript/components/shared/Header.jsx
+++ app/javascript/components/shared/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => (
-  <nav className="navbar navbar-expand-lg navbar-light ">
-    <a className="navbar-brand goog" href="index.html">O-Sale</a>
+  <nav className="navbar navbar-expand-lg navbar-light">
+    <Link to='/' className="navbar-brand goog"> O-Sale </Link>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -10,7 +11,8 @@ const Header = () => (
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav ml-auto">
         <li className="nav-item">
-          <a className="nav-link" href="index.html">Home <span className="sr-only">(current)</span></a>
+        <Link to='/' className="nav-link">Home <span className="sr-only">(current)</span></Link>
+
         </li>
         <li className="nav-item">
           <a className="nav-link" href="new-item.html">New Product</a>
