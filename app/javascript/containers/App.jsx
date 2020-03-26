a61f65ec20fc537532f3cf08b00b9395c0999915
Dragon1207 <smileboth0805@outlook.com>
2020-03-26 14:05:09 -0700

    Add 404 not-found component

--- app/javascript/containers/App.jsx
+++ app/javascript/containers/App.jsx
@@ -11,7 +11,18 @@ const App = () => (
     <React.Fragment>
       <Header />
       <Switch>
-        <Route exact path='/' component={ProductList}/>        
+        <Route exact path='/' component={ProductList}/>
+
+        <Route render={() => (
+          <div className="container">
+            <div className="row">
+              <div className="card col-md-8 offset-md-2 mt-4 form-header-style">
+                <h1 className="text-center m-4">404: Not Found</h1>
+                <p className="text-center m-4">The resource you are looking for could not be found</p>
+              </div>
+            </div>
+          </div>
+        )} />
       </Switch>
       <Footer />
     </React.Fragment>
